9d59334b784ca498e603985b4095f92b377590bd
c0de4un <code4un@yandex.ru>
2020-03-04 14:27:09 +0400

    (feat) hello world + project-struct

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # doc-to-img-cv
 DocToImg.CV - console-utility for converting image of document to a rotated document-scan-like image.
+
+# System requirements
+OS: Windows || Linux, or any other with C++ GCC || Clang || MSVC support;
+ABI: x86-32 or x86-64 only guarantee;
+
+# Used Tools
+Tools:
+ - QTCreator IDE + CMake (Ninja generator);
+
+# Structure
+ - bin contains compiled binaries;
+ - external contains third-party sources/libs;
+ - libs - contains pre-compiled libraries;
+ - src contains source-code (cxx for C++, java, kt etc);
+ - cmake contains cmake-script include-files;
+
+# Campatability-List
+Supported build-tools:
+ - GNU GCC 7.3.0;
+ - MSVC 14+;
---
+++ src/cxx/itdcv/win/main.hpp
@@ -0,0 +1,36 @@
+/**
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS
+* IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+* THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+* PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS OR CONTRIBUTORS
+* BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+* CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+* POSSIBILITY OF SUCH DAMAGE.
+**/
+
+#ifndef C0DE4UN_ITDCV_WIN_MAIN_HPP
+#define C0DE4UN_ITDCV_WIN_MAIN_HPP
+
+// -----------------------------------------------------------
+
+// ===========================================================
+// INCLUDES
+// ===========================================================
+
+// C++ I/O
+#include <iostream>
+
+// ===========================================================
+// CONSTANTS & FIELDS
+// ===========================================================
+
+/** C++ 11+ Feature. **/
+static constexpr const int itdcv_OK = 0;
+
+// -----------------------------------------------------------
+
+#endif // !C0DE4UN_ITDCV_WIN_MAIN_HPP
